787f8c43a58841c4963c067fd1272a56e4972a6e
SyedGhullamMeeranGillani <meergillani999@icloud.com>
2022-07-27 12:32:21 +0500

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Syed Ghullam Meeran Gillani on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
