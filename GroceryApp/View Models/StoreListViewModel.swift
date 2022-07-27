e35a01ae28b125cf0fb8e528915ff27255168395
SyedGhullamMeeranGillani <meergillani999@icloud.com>
2022-07-27 15:11:47 +0500

    Implementing StoreListViewModel class to get data and and create StoreViewModel Structure to convert the data.

--- GroceryApp/View Models/StoreListViewModel.swift
+++ GroceryApp/View Models/StoreListViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+class StoreListViewModel: ObservableObject {
+    private var firestoreManager: FirestoreManager
+    
+    @Published var stores: [StoreViewModel] = []
+    
+    init(){
+        firestoreManager = FirestoreManager()
+    }
+    
+    func getAll(){
+        firestoreManager.getAllStores { result in
+            switch result {
+            case .success(let stores):
+                if let store = stores {
+                    DispatchQueue.main.async {
+                        self.stores = store.map(StoreViewModel.init)
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+ }
+struct StoreViewModel {
+    let store : Store
+    
+    var storeId: String{
+        store.id ?? ""
+    }
+    var name: String{
+        store.name
+    }
+    var address: String{
+        store.address
+    }
+}
